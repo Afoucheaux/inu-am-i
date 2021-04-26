1e8511644b729d28f158eb3886039d9e88aa0994
Aaron Foucheaux <afoucheaux@gmail.com>
2021-04-26 16:27:51 -0500

    Small formating changes

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -4,7 +4,6 @@ const catBaseUrl = 'https://pure-hollows-05817.herokuapp.com/http://shibe.online
 export const getGameImages = (number) => {
   const numberOfCats = number * 2;
   const apiData = [fetch(`${shibaBaseURL}?count=${number}&urls=true&httpsUrls=true`), fetch(`${catBaseUrl}?count=${numberOfCats}&urls=true&httpsUrls=true`)];
-
   return Promise.all(apiData)
   .then(responses => Promise.all(responses.map(response => response.json())))
   }
