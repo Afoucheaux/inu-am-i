db51190195473944d37ff793856c5b479102bf43
Aaron Foucheaux <afoucheaux@gmail.com>
2021-05-02 16:44:22 -0500

    1

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -4,6 +4,7 @@ Cypress.Commands.add('seedAndVisitDefault', () => {
   cy.visit('http://localhost:3000')
 })
 
+
 Cypress.Commands.add('seedAndVisitInput', () => {
   cy.intercept('https://pure-hollows-05817.herokuapp.com/http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=true', {fixture:'shibasInput.js'})
   cy.intercept('https://pure-hollows-05817.herokuapp.com/http://shibe.online/api/cats?count=2&urls=true&httpsUrls=true', {fixture:'catsInput.js'})
