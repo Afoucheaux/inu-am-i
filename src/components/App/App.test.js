32ef3e1a1201ca04e7325ddf29eeb94c073f5122
Aaron Foucheaux <afoucheaux@gmail.com>
2021-05-02 10:44:24 -0500

    bug fix

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import {BrowserRouter} from 'react-router-dom';
 
 test('renders learn react link', () => {
   render(
